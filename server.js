33ebf80b13b675c616a972d0e3b958b0ee4ef196
Matt Gould <gouldie70@gmail.com>
2016-11-13 14:46:05 +0000

    server setup

--- server.js
+++ server.js
@@ -3,4 +3,6 @@ var app = express();
 
 app.use(express.static(__dirname + '/'));
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+  console.log('Listening..')
+});
