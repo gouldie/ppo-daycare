a13619fa31468eb2cad8386f1c42e2a49e086689
Matt Gould <gouldie70@gmail.com>
2016-11-17 21:26:49 +0000

    style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,20 +25,21 @@ export default class App extends Component {
         <Calc update={this.mapStorageToState} />
         <JobList update={this.mapStorageToState} />
 
-      <div style={{
-        position: 'absolute',
-        bottom: '30px',
-        left: '0',
-        right: '0',
-        marginLeft: 'auto',
-        marginRight: 'auto'
-      }}>
-        <p style={{ marginBottom: '5px' }}>Made with ♥ by</p>
-        <img
-          src={'http://i.imgur.com/Y5QiBTf.png'}
-          alt="boohoo"
-          ></img>
-          </div>
+        <div className='footer' style={{
+          position: 'absolute',
+          left: '0',
+          right: '0',
+          marginLeft: 'auto',
+          marginRight: 'auto'
+        }}>
+          <p style={{ marginBottom: '5px' }}>Made with ♥ by</p>
+          <img
+            src={'http://i.imgur.com/Y5QiBTf.png'}
+            alt="boohoo"
+            >
+          </img>
+          <p style={{ margin: '5px 0 0 0' }}>comments, suggestions, criticism welcome :^)</p>
+        </div>
       </div>
     )
   }
