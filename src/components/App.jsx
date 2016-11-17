a3df85fc286973e3c6a9c40baf01d752ec016c6a
Matt Gould <gouldie70@gmail.com>
2016-11-17 20:55:01 +0000

    finalize

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,21 +20,25 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className='container'>
+      <div>
         <h1>PPO Daycare Buddy<span style={{fontSize: '10px', verticalAlign: 'top'}}>beta</span></h1>
         <Calc update={this.mapStorageToState} />
         <JobList update={this.mapStorageToState} />
+
+      <div style={{
+        position: 'absolute',
+        bottom: '30px',
+        left: '0',
+        right: '0',
+        marginLeft: 'auto',
+        marginRight: 'auto'
+      }}>
+        <p style={{ marginBottom: '5px' }}>Made with ♥ by Gouldie</p>
         <img
           src={'http://i.imgur.com/Y5QiBTf.png'}
           alt="boohoo"
-          style={{
-            position: 'absolute',
-            bottom: '30px',
-            left: '0',
-            right: '0',
-            marginLeft: 'auto',
-            marginRight: 'auto'
-          }}></img>
+          ></img>
+          </div>
       </div>
     )
   }
