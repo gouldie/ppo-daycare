00fc47df704396143520149bdc51a2d5aee978a4
Matt <mgould@breakingfreegroup.com>
2017-06-07 15:14:35 +0100

    links updated for gh-pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,7 +21,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>PPO Daycare Buddy<span style={{fontSize: '10px', verticalAlign: 'top'}}>beta</span></h1>
+        <h1>PPO Daycare Buddy</h1>
         <Calc update={this.mapStorageToState} />
         <JobList update={this.mapStorageToState} />
 
